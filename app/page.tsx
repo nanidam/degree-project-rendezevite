e7a9d19c95ff9682f9941ac7be1f7c8739281f9b
nanidam <nani.dam@medieinstitutet.se>
2024-01-02 22:54:40 +0100

    :heavy_plus_sign: added next-auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,20 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1>test</h1>
+      <section>
+        <h1>Welcome to RendezEvite</h1>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque ea ab pariatur
+          nostrum ipsum quis doloribus eveniet libero iste qui molestias quos, cumque,
+          nesciunt mollitia exercitationem repudiandae! Ex, tempore nihil?
+        </p>
+      </section>
+      <section>
+        <a href="/">Login</a>
+        <a href="/">Register</a>
+      </section>
     </main>
   );
 }
