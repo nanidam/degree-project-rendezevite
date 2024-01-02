f7c1b1e7adb46d43a686b91f254083663bf322d7
nanidam <nani.dam@medieinstitutet.se>
2024-01-02 22:56:20 +0100

    uncomment newUser test

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -3,12 +3,12 @@ import prisma from "../db";
 export default async function Testing() {
   console.log("hello");
 
-  const newUser = await prisma.user.create({
-    data: {
-      email: "hakllå",
-      hashedPassword: "test",
-    },
-  });
+  // const newUser = await prisma.user.create({
+  //   data: {
+  //     email: "hakllå",
+  //     hashedPassword: "test",
+  //   },
+  // });
   return (
     <>
       <h1>About page</h1>
