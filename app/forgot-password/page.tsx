a123f4410cfe34e640cccda8366e6fdc49c0938e
nanidam <nani.dam@medieinstitutet.se>
2024-01-04 19:50:48 +0100

    added form

--- app/forgot-password/page.tsx
+++ app/forgot-password/page.tsx
@@ -1,21 +1,27 @@
+"use client";
 import "./style.scss";
 
 const ForgotPassword = () => {
+  const handleForgotPassword = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("Forgot Password clicked");
+  };
+
   return (
     <section className="forgot-password-container">
       <h1>Forgot Password</h1>
       <p>Enter your email address to reset your password. You will recieve an email.</p>
-      <label htmlFor="forgot-password">Email:</label>
-      <input
-        className="forgot-input"
-        type="email"
-        id="forgot-password"
-        name="forgot-password"
-        placeholder="Your email"
-      />
-      <button className="reset-password-btn" type="submit">
-        Reset Password
-      </button>
+      <form className="forgot-password-form" onClick={handleForgotPassword}>
+        <label htmlFor="forgot-password">Email:</label>
+        <input
+          className="forgot-input"
+          type="email"
+          id="forgot-password"
+          name="forgot-password"
+          placeholder="Your email"
+        />
+        <button className="reset-password-btn">Reset Password</button>
+      </form>
     </section>
   );
 };
