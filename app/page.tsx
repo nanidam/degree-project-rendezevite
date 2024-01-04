ae0f839dcc78255c67b1c895f63d7222b585f1ab
nanidam <nani.dam@medieinstitutet.se>
2024-01-04 15:32:31 +0100

    :art: centered text

--- app/page.tsx
+++ app/page.tsx
@@ -14,21 +14,23 @@ const Home = () => {
 
   return (
     <main>
-      <section className="welcome-overview">
+      <section className="welcome-container">
         <h1 className="welcome-header">Welcome to RendezEvite</h1>
-        <p className="welcome-text">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque ea ab pariatur
-          nostrum ipsum quis doloribus eveniet libero iste qui molestias quos, cumque,
-          nesciunt mollitia exercitationem repudiandae! Ex, tempore nihil?
-        </p>
-      </section>
-      <section className="login-register-links">
-        <button className="login-btn" type="button" onClick={handleLogin}>
-          Login
-        </button>
-        <button className="register-btn" type="button" onClick={handleRegister}>
-          Register
-        </button>
+        <article className="welcome-text-container">
+          <p className="welcome-text">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque ea ab pariatur
+            nostrum ipsum quis doloribus eveniet libero iste qui molestias quos, cumque,
+            nesciunt mollitia exercitationem repudiandae! Ex, tempore nihil?
+          </p>
+        </article>
+        <article className="welcome-options-container">
+          <button className="login-btn" type="button" onClick={handleLogin}>
+            Login
+          </button>
+          <button className="register-btn" type="button" onClick={handleRegister}>
+            Register
+          </button>
+        </article>
       </section>
     </main>
   );
