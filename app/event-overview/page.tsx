7d535e69d4cd2b7afeae6d8ea7cc0e3b55a52497
nanidam <nani.dam@medieinstitutet.se>
2024-01-05 17:05:05 +0100

    added basic event overview html

--- app/event-overview/page.tsx
+++ app/event-overview/page.tsx
@@ -1,15 +1,41 @@
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "../api/auth/[...nextauth]/authOptions";
 import Logout from "../components/logout";
+import "./style.scss";
 
 const EventOverview = async () => {
   const session = await getServerSession(authOptions);
   console.log(session);
+
   return (
-    <div>
-      <h1>Event Overview</h1>
+    <main>
+      <h1 className="event-overview-header">
+        Event <br />
+        Overview
+      </h1>
       <Logout></Logout>
-    </div>
+      <section className="event-overview-container">
+        <article className="create-event">
+          <button className="create-event-btn">Create event</button>
+        </article>
+
+        <article className="current-events">
+          <h2>Current events:</h2>
+          <ul>
+            <li>event 1</li>
+            <li>event 2</li>
+          </ul>
+        </article>
+
+        <article className="past-events">
+          <h2>Past events:</h2>
+          <ul>
+            <li>event 3</li>
+            <li>event 4</li>
+          </ul>
+        </article>
+      </section>
+    </main>
   );
 };
 
