dec02621a3e7c361b85bb663e125071f78f56bf9
nanidam <nani.dam@medieinstitutet.se>
2024-01-05 17:29:35 +0100

    refactor

--- app/components/logout.tsx
+++ app/components/logout.tsx
@@ -2,11 +2,8 @@
 import { ReactSVG } from "react-svg";
 import "./style/logoutBtn.scss";
 import { signOut } from "next-auth/react";
-import { useRouter } from "next/navigation";
 
 const Logout = () => {
-  const router = useRouter();
-
   const handleLogout = async () => {
     await signOut({ callbackUrl: "/" });
   };
