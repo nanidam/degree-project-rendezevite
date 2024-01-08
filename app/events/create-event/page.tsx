b4a3bdc894bb50aaea070a7dfe1f1abc0ecf48da
nanidam <nani.dam@medieinstitutet.se>
2024-01-08 20:53:38 +0100

    create event validations +  display error msg

--- app/events/create-event/page.tsx
+++ app/events/create-event/page.tsx
@@ -1,14 +1,47 @@
 "use client";
 
+import { useState } from "react";
 import "./style.scss";
 
+const CREATE_EVENT_STATUS = {
+  EMPTY_NAME: "Please give your event a name",
+  EMPTY_DATE: "Please choose a date",
+  GENERIC: "Something went wrong. Please try again",
+  INVALID: "Invalid date",
+  SUCCESS: "Event created",
+};
+
 const CreateEvent = () => {
+  const [errorMsg, setErrorMsg] = useState<string | null>(null);
   const handleCreateEvent = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const formData = new FormData(e.currentTarget);
     const eventName = formData.get("event-name") as string;
     const eventDate = formData.get("event-date") as string;
     console.log(eventName, eventDate);
+
+    switch (true) {
+      case eventName === "":
+        setErrorMsg(CREATE_EVENT_STATUS.EMPTY_NAME);
+        break;
+
+      case eventDate === "":
+        setErrorMsg(CREATE_EVENT_STATUS.EMPTY_DATE);
+        break;
+
+      case new Date(eventDate) < new Date():
+        setErrorMsg(CREATE_EVENT_STATUS.INVALID);
+        break;
+
+      case eventDate !== "" && eventName !== "":
+        setErrorMsg(CREATE_EVENT_STATUS.SUCCESS);
+        break;
+
+      default:
+        setErrorMsg(CREATE_EVENT_STATUS.GENERIC);
+        break;
+    }
+
     // const userId = await getUserId()
 
     // if (userId) {
@@ -25,6 +58,7 @@ const CreateEvent = () => {
     //   }
     // }
   };
+
   return (
     <section className="create-event-container">
       <h1 className="create-event-header">Create Event</h1>
@@ -46,6 +80,7 @@ const CreateEvent = () => {
             Event date:
             <input className="create-event-input" type="date" name="event-date" />
           </label>
+          {errorMsg && <span className="error-message">{errorMsg}</span>}
           <button className="submit-event-btn" type="submit">
             Submit
           </button>
