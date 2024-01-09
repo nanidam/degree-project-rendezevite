e98ba3445c24ff2043e8698d89159e7078109346
nanidam <nani.dam@medieinstitutet.se>
2024-01-09 20:42:56 +0100

    pick template

--- app/events/create-event/[eventName]/template/page.tsx
+++ app/events/create-event/[eventName]/template/page.tsx
@@ -1,3 +1,9 @@
+"use client";
+
+import Logout from "@/app/components/logout";
+import "./style.scss";
+import chooseTemplate from "@/app/services/chooseTemplate";
+
 const Template = ({
   params: { eventName },
 }: {
@@ -6,10 +12,34 @@ const Template = ({
   if (eventName.includes(".")) {
     return null;
   }
+
+  const choosingTemplate = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const template = formData.get("options") as string;
+    chooseTemplate(template, eventName);
+  };
   return (
-    <>
-      <h1>Choose template</h1>
-    </>
+    <section className="choose-template-container">
+      <h1 className="choose-template-header">Choose template</h1>
+      <Logout />
+      <article className="choose-template">
+        <form onSubmit={choosingTemplate} className="choose-template-form">
+          <label htmlFor="options">Select a template:</label>
+          <select className="template-options" id="options" name="options">
+            <option value="templateGeoDesign">Geometric</option>
+            <option value="templateFlowerDesign">Flowers</option>
+          </select>
+
+          <article className="preview-template">
+            <div>Preview?</div>
+          </article>
+          <button className="template-btn" type="submit">
+            Confirm
+          </button>
+        </form>
+      </article>
+    </section>
   );
 };
 
