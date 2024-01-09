506851ebffd7c984fbec12d07340478f1e8e36c6
nanidam <nani.dam@medieinstitutet.se>
2024-01-09 20:44:01 +0100

    check existing event names

--- app/services/createEvent.ts
+++ app/services/createEvent.ts
@@ -2,23 +2,26 @@
 
 import prisma from "../db";
 import { IEvent } from "../models/IEvent";
+import { getEvent } from "./getEvent";
 
 const createEvent = async ({ eventDate, eventName, userId, eventPassword }: IEvent) => {
-  try {
-    const newEvent = await prisma.event.create({
-      data: {
-        eventName,
-        eventDate: new Date(eventDate),
-        userId,
-        eventPassword,
-      },
-    });
-
-    return newEvent;
-  } catch (error) {
-    console.error("Error creating event:", error);
+  const existingEvent = await getEvent(userId, eventName);
+  if (existingEvent) {
+    console.error("Error creating event:", "Event already exists");
     return null;
   }
+
+  const newEvent = await prisma.event.create({
+    data: {
+      eventName,
+      eventDate: new Date(eventDate),
+      userId,
+      eventPassword,
+    },
+  });
+
+  console.log("Created event:", newEvent);
+  return newEvent;
 };
 
 export default createEvent;
