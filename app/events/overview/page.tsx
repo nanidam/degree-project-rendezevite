3b9181ad2e9654d6bb7909b8c9276470ee451dad
Nani Dam <nani7dam@gmail.com>
2024-01-15 22:11:33 +0100

    :sparkles: show correct events from db + delete event

--- app/events/overview/page.tsx
+++ app/events/overview/page.tsx
@@ -1,22 +1,39 @@
-"use client";
+"use client"
 
-import { getServerSession } from "next-auth/next";
-import { authOptions } from "../../api/auth/[...nextauth]/authOptions";
-import Logout from "../../components/logout";
-import "./style.scss";
-import { ReactSVG } from "react-svg";
-import Link from "next/link";
-import { useRouter } from "next/navigation";
+import { getServerSession } from "next-auth/next"
+import { authOptions } from "../../api/auth/[...nextauth]/authOptions"
+import Logout from "../../components/logout"
+import "./style.scss"
+import { ReactSVG } from "react-svg"
+import Link from "next/link"
+import { useRouter } from "next/navigation"
+import { useEffect, useState } from "react"
+import { getAllEvents } from "@/app/services/getAllEvents"
+import { eventNames } from "process"
+import { deleteEvent } from "@/app/services/deleteEvent"
 
 const EventOverview = () => {
-  // const session = await getServerSession(authOptions);
-  // console.log(session);
-  const router = useRouter();
+  const [events, setEvents] = useState<string[]>([])
+  const fetchAndSetEvents = async () => {
+    const data = await getAllEvents()
+    if (data) {
+      setEvents(data.map((event) => event.eventName))
+    }
+  }
+
+  useEffect(() => {
+    fetchAndSetEvents()
+  }, [])
+  const router = useRouter()
 
   const createEvent = () => {
-    router.push("/events/create-event");
-  };
+    router.push("/events/create-event")
+  }
 
+  const handleDeleteEvent = (eventName: string) => {
+    deleteEvent(eventName)
+    fetchAndSetEvents()
+  }
   return (
     <main>
       <h1 className="event-overview-header">
@@ -34,24 +51,20 @@ const EventOverview = () => {
         <article className="current-events-container">
           <h2>Current events:</h2>
           <ul className="current-events">
-            <li className="current-event">
-              <Link className="event" href="/">
-                event 1
-              </Link>
-              <div className="edit-delete-icons">
-                <ReactSVG className="edit-icon" src="/edit-icon.svg"></ReactSVG>
-                <ReactSVG className="trash-icon" src="/trash-can.svg"></ReactSVG>
-              </div>
-            </li>
-            <li className="current-event">
-              <Link className="event" href="/">
-                event 2
-              </Link>
-              <div className="edit-delete-icons">
-                <ReactSVG className="edit-icon" src="/edit-icon.svg"></ReactSVG>
-                <ReactSVG className="trash-icon" src="/trash-can.svg"></ReactSVG>
-              </div>
-            </li>
+            {events.map((eventName) => (
+              <li key={eventName} className="current-event">
+                <Link className="event" href={`/admin/overview/${eventName}`}>
+                  {eventName}
+                </Link>
+                <div className="edit-delete-icons">
+                  <ReactSVG
+                    className="trash-icon"
+                    src="/trash-can.svg"
+                    onClick={() => handleDeleteEvent(eventName)}
+                  />
+                </div>
+              </li>
+            ))}
           </ul>
         </article>
 
@@ -64,7 +77,7 @@ const EventOverview = () => {
         </article>
       </section>
     </main>
-  );
-};
+  )
+}
 
-export default EventOverview;
+export default EventOverview
