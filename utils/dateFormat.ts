bce4ed821e766f09c92c8b37549b6c7eb3b1a15a
Nani Dam <nani7dam@gmail.com>
2024-01-15 22:02:45 +0100

    format date

---
+++ utils/dateFormat.ts
@@ -0,0 +1,5 @@
+export const dateFormat = (date: Date) => date.getFullYear() +
+    "-" +
+    (date.getMonth() + 1).toString().padStart(2, "0") +
+    "-" +
+    date.getDate().toString().padStart(2, "0")
