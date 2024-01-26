ce328f2ddb0dfa17a7d6f14dab2afb72af749a10
nanidam <nani.dam@medieinstitutet.se>
2024-01-26 16:59:59 +0100

    refactoring

--- app/events/overview/page.tsx
+++ app/events/overview/page.tsx
@@ -1,43 +1,40 @@
-"use client"
+"use client";
 
-import { getServerSession } from "next-auth/next"
-import { authOptions } from "../../api/auth/[...nextauth]/authOptions"
-import Logout from "../../components/logout"
-import "./style.scss"
-import { ReactSVG } from "react-svg"
-import Link from "next/link"
-import { useRouter } from "next/navigation"
-import { useEffect, useState } from "react"
-import { getAllEvents } from "@/app/services/getAllEvents"
-import { eventNames } from "process"
-import { deleteEvent } from "@/app/services/deleteEvent"
+import Logout from "../../components/logout";
+import "./style.scss";
+import { ReactSVG } from "react-svg";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import { getAllEvents } from "@/app/services/getAllEvents";
+import { deleteEvent } from "@/app/services/deleteEvent";
 
 const EventOverview = () => {
-  const [events, setEvents] = useState<string[]>([])
+  const [events, setEvents] = useState<string[]>([]);
   const fetchAndSetEvents = async () => {
-    const data = await getAllEvents()
+    const data = await getAllEvents();
     if (data) {
-      setEvents(data.map((event) => event.eventName))
+      setEvents(data.map((event) => event.eventName));
     }
-  }
+  };
 
   useEffect(() => {
-    fetchAndSetEvents()
-  }, [])
-  const router = useRouter()
+    fetchAndSetEvents();
+  }, []);
+  const router = useRouter();
 
   const createEvent = () => {
-    router.push("/events/create-event")
-  }
+    router.push("/events/create-event");
+  };
 
   const handleDeleteEvent = (eventName: string) => {
-    deleteEvent(eventName)
-    fetchAndSetEvents()
-  }
+    deleteEvent(eventName);
+    fetchAndSetEvents();
+  };
   return (
     <main>
       <h1 className="event-overview-header">
-        Event <br />
+        Events <br />
         Overview
       </h1>
       <Logout></Logout>
@@ -48,36 +45,38 @@ const EventOverview = () => {
           </button>
         </article>
 
-        <article className="current-events-container">
-          <h2>Current events:</h2>
-          <ul className="current-events">
-            {events.map((eventName) => (
-              <li key={eventName} className="current-event">
-                <Link className="event" href={`/admin/overview/${eventName}`}>
-                  {eventName}
-                </Link>
-                <div className="edit-delete-icons">
-                  <ReactSVG
-                    className="trash-icon"
-                    src="/trash-can.svg"
-                    onClick={() => handleDeleteEvent(eventName)}
-                  />
-                </div>
-              </li>
-            ))}
-          </ul>
-        </article>
+        <div className="event-wrapper">
+          <article className="current-events-container">
+            <h2>Current events:</h2>
+            <ul className="current-events">
+              {events.map((eventName) => (
+                <li key={eventName} className="current-event">
+                  <Link className="event" href={`/admin/overview/${eventName}`}>
+                    {eventName}
+                  </Link>
+                  <div className="edit-delete-icons">
+                    <ReactSVG
+                      className="trash-icon"
+                      src="/trash-can.svg"
+                      onClick={() => handleDeleteEvent(eventName)}
+                    />
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </article>
 
-        <article className="past-events-container">
-          <h2>Past events:</h2>
-          <ul>
-            <li>event 3</li>
-            <li>event 4</li>
-          </ul>
-        </article>
+          <article className="past-events-container">
+            <h2>Past events:</h2>
+            <ul>
+              <li>event 3</li>
+              <li>event 4</li>
+            </ul>
+          </article>
+        </div>
       </section>
     </main>
-  )
-}
+  );
+};
 
-export default EventOverview
+export default EventOverview;
