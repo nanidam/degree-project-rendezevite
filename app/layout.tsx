1d88d7831ae55adbcb4313e949e31f1f5ebf47d4
nanidam <nani.dam@medieinstitutet.se>
2024-01-29 23:53:31 +0100

    :truck: changing file structure, moving files

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import Providers from "./components/Providers";
+import Providers from "./utils/components/Providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
