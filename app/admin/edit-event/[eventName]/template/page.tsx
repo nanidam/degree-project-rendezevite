ae1ce7ca00e5814b826c2d6a8965d2faa85d4f05
Nani Dam <nani7dam@gmail.com>
2024-02-01 23:09:48 +0100

    :construction: WIP edit template

---
+++ app/admin/edit-event/[eventName]/template/page.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import chooseTemplate from "@/app/services/chooseTemplate";
+import { useRouter } from "next/navigation";
+
+const Template = ({
+  params: { eventName },
+}: {
+  readonly params: { readonly eventName: string };
+}) => {
+  const router = useRouter();
+
+  if (eventName.includes(".")) {
+    return null;
+  }
+
+  const choosingTemplate = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const template = formData.get("options") as string;
+    chooseTemplate(template, eventName);
+
+    router.push(`/events/create-event/${eventName}/invitations`);
+  };
+  return (
+    <section className="choose-template-container">
+      <h1 className="choose-template-header">
+        Choose your <br className="choose-template-br" />
+        template
+      </h1>
+      <article className="choose-template">
+        <form onSubmit={choosingTemplate} className="choose-template-form">
+          <h2>
+            Template:
+            <select className="template-options" id="options" name="options">
+              <option className="template-option" value="templateGeoDesign">
+                Geometric
+              </option>
+              <option className="template-option" value="templateFlowerDesign">
+                Flowers
+              </option>
+            </select>
+          </h2>
+
+          <div className="preview-template">
+            <div>Preview?</div>
+          </div>
+          <button className="template-btn" type="submit">
+            Next -{">"}
+          </button>
+        </form>
+      </article>
+    </section>
+  );
+};
+
+export default Template;
