cb056b603f8fbdb1e216120a691894510278efb0
nanidam <nani.dam@medieinstitutet.se>
2024-02-01 16:59:37 +0100

    added support page

--- app/support/page.tsx
+++ app/support/page.tsx
@@ -1,8 +1,32 @@
+"use client";
+
+import { ReactSVG } from "react-svg";
+import "./style.scss";
+
 const Support = () => {
   return (
-    <>
-      <h1>Support page</h1>
-    </>
+    <section className="support">
+      <h1>Support Page</h1>
+      <article className="support-wrapper">
+        <p className="support-text">
+          If you encounter any issues with the application, please don&apos;t hesitate to
+          contact our {""}
+          <a href="mailto:rendezevite@gmail.com">support</a>.
+        </p>
+        <div className="support-svgs">
+          <ReactSVG src="/svgs/settings.svg" />
+          <ReactSVG src="/svgs/setting-tools.svg" />
+        </div>
+
+        <button
+          className="support-btn-return"
+          type="button"
+          onClick={() => window.history.back()}
+        >
+          Return
+        </button>
+      </article>
+    </section>
   );
 };
 
