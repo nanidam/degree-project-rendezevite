a356f05b04ea53a695aed22177c9f5d052e563b4
Nani Dam <nani7dam@gmail.com>
2024-02-02 22:16:13 +0100

    created edit RSVP page

---
+++ app/admin/edit-event/[eventName]/edit-RSVP/page.tsx
@@ -0,0 +1,13 @@
+const EditRsvp = async ({
+  params: { eventName },
+}: {
+  params: { eventName: string };
+}) => {
+  return (
+    <div>
+      <h1>Edit RSVP</h1>
+    </div>
+  );
+};
+
+export default EditRsvp;
