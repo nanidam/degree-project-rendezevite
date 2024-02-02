ff41d3e4cd2f17c20187ed556617384a572fdb2f
Nani Dam <nani7dam@gmail.com>
2024-02-02 22:38:54 +0100

    render rsvp

--- app/admin/edit-event/[eventName]/edit-RSVP/page.tsx
+++ app/admin/edit-event/[eventName]/edit-RSVP/page.tsx
@@ -1,13 +1,19 @@
-const EditRsvp = async ({
+import { getEvent } from "@/app/services/getEvent";
+import getUserId from "@/app/services/getUserId";
+import EditRsvp from "@/app/utils/components/EditRsvp";
+
+const EditRsvpPage = async ({
   params: { eventName },
 }: {
   params: { eventName: string };
 }) => {
-  return (
-    <div>
-      <h1>Edit RSVP</h1>
-    </div>
-  );
+  const userId = await getUserId();
+  if (!userId) return null;
+
+  const event = await getEvent(userId, eventName);
+  if (!event) return null;
+
+  return <EditRsvp eventName={eventName} event={event} />;
 };
 
-export default EditRsvp;
+export default EditRsvpPage;
---
+++ app/utils/createEditRsvp.ts
@@ -0,0 +1,28 @@
+import { useRouter } from "next/navigation";
+import updateEventFood from "../services/updateEventFood";
+
+interface ICreateEditRsvp {
+    e: React.FormEvent<HTMLFormElement>
+    eventName: string;
+    router: ReturnType<typeof useRouter>
+}
+const createEditRsvp = async ({ e, eventName, router }: ICreateEditRsvp) => {
+    e.preventDefault();
+    const form = e.target as HTMLFormElement;
+    const formData = new FormData(form);
+
+    const includeFood = formData.get("ask-about-food") === "true";
+    const includeAllergies = formData.get("ask-about-allergies") === "true";
+
+    const updatedEvent = await updateEventFood(
+        includeFood,
+        includeAllergies,
+        eventName.toLocaleLowerCase()
+    );
+
+    if (updatedEvent) {
+        router.push(`/admin/overview/${eventName}`);
+    }
+};
+
+export default createEditRsvp
