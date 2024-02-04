b72e2ed5c847669180d2470e88ba73d3a32562df
Nani Dam <nani7dam@gmail.com>
2024-02-04 15:06:35 +0100

    added guest welcome page

--- app/utils/components/guestWelcome.tsx
+++ app/utils/components/guestWelcome.tsx
@@ -1,14 +1,51 @@
 "use client";
-import { useSession } from "next-auth/react";
 import { IEvent } from "../models/IEvent";
+import { ISession } from "../models/ISession";
+import { useRouter } from "next/navigation";
+import "./style/guestWelcome.scss";
 
 interface IGuestWelcomeProps {
   event: IEvent;
+  session: ISession | null;
 }
-const GuestWelcome = ({ event }: IGuestWelcomeProps) => {
-  const session = useSession();
-  console.log(session.data?.user);
-  return <div>Welcome</div>;
+
+// TODO: Only send in relevant info, check if event.guestlist contains all guests. if so remove.
+const GuestWelcome = ({ event, session }: IGuestWelcomeProps) => {
+  const router = useRouter();
+  return (
+    <>
+      <section className="wrapper-invitation">
+        <h1 className="welcome-title">Welcome to {event.eventName}</h1>
+        <h4 className="sub-title">{event.eventDate}</h4>
+        <p className="welcome-text">A special invitation to:</p>
+        <span className="welcome-span">
+          <p className="guest-name"> {event.guestList[0].name}</p>
+          {event.guestList[0].additionalGuest.name.length > 0 && (
+            <>
+              <p className="additional-guest-prefix"> &</p>
+              <p className="additional-guest-name">
+                {event.guestList[0].additionalGuest.name}
+              </p>
+            </>
+          )}
+        </span>
+        <div className="next-return-wrapper">
+          <button className="submit-event-btn" type="button" disabled>
+            Previous
+          </button>
+          <button
+            className="submit-event-btn"
+            type="button"
+            onClick={() => {
+              router.push(`/invitation/${event.id}/${event.eventName}`);
+            }}
+          >
+            Next
+          </button>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default GuestWelcome;
