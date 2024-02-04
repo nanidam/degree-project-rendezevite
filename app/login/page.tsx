199afb39284ee0c44e613dbadd1638af3bb135fc
Nani Dam <nani7dam@gmail.com>
2024-02-04 13:14:14 +0100

    :fire: removed login function

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,12 +1,9 @@
 "use client";
 
 import LoginRegisterForm from "../utils/components/loginRegisterForm";
-import login from "../utils/login";
 
 const Login = () => {
-  return (
-    <LoginRegisterForm loginRegisterHeader="Login" handleSubmit={login} loginType="admin" />
-  );
+  return <LoginRegisterForm loginRegisterHeader="Login" loginType="admin" />;
 };
 
 export default Login;
