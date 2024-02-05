1c5e0d51ecf81f06aa3311066028fd9a22aafda0
Nani Dam <nani7dam@gmail.com>
2024-02-05 15:20:06 +0100

    commented out cuz :bug:

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/:path*',
-        destination: '/not-found',
-      },
-    ];
-}
+//   async rewrites() {
+//     return [
+//       {
+//         source: '/:path*',
+//         destination: '/not-found',
+//       },
+//     ];
+// }
   };
-
+// TODO: Page not found redirect
   module.exports = nextConfig;
