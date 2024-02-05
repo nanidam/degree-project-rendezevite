fd825e80298a1adbbde98c28923fb9917cfddb88
nanidam <nani.dam@medieinstitutet.se>
2024-02-06 00:39:10 +0100

    :art: fit most mobile sizes

--- app/utils/components/templateGeoDesign.tsx
+++ app/utils/components/templateGeoDesign.tsx
@@ -1,8 +1,9 @@
 "use client";
 
+import "./style/templateGeoDesign.scss";
+import { ReactSVG } from "react-svg";
 import { useRouter } from "next/navigation";
 import { ITemplateGeometricDesignProps } from "../models/ITemplateGeometricDesign";
-import "./style/templateGeoDesign.scss";
 
 const TemplateGeometricDesign: React.FC<ITemplateGeometricDesignProps> = ({
   header,
@@ -79,30 +80,40 @@ const TemplateGeometricDesign: React.FC<ITemplateGeometricDesignProps> = ({
 
   return (
     <section className="template-bg">
-      <h1 className="template-header">{header}</h1>
-      <article className="template-text-container">
-        <p className="template-text">{text}</p>
+      <article className="template-container">
+        <h1 className="template-header">{header}</h1>
+        <div className="template-text-container">
+          <p className="template-text">{text}</p>
+        </div>
+        <div className="next-return-wrapper">
+          <button
+            className="invitation-btn"
+            type="button"
+            onClick={() => {
+              router.push(`/invitation/${eventId}/welcome`);
+            }}
+          >
+            <ReactSVG
+              className="invitation-navigation-arrow"
+              src="/svgs/arrow.svg"
+              aria-label="Return"
+            ></ReactSVG>
+          </button>
+          <button
+            className="invitation-btn"
+            type="button"
+            onClick={() => {
+              router.push(`/invitation/${eventId}/rsvp`);
+            }}
+          >
+            <ReactSVG
+              className="invitation-navigation-arrow-left"
+              src="/svgs/arrow.svg"
+              aria-label="Next"
+            ></ReactSVG>
+          </button>
+        </div>
       </article>
-      <div className="next-return-wrapper">
-        <button
-          className="submit-event-btn"
-          type="button"
-          onClick={() => {
-            router.push(`/invitation/${eventId}/welcome`);
-          }}
-        >
-          Previous
-        </button>
-        <button
-          className="submit-event-btn"
-          type="button"
-          onClick={() => {
-            router.push(`/invitation/${eventId}/rsvp`);
-          }}
-        >
-          Next
-        </button>
-      </div>
     </section>
   );
 };
