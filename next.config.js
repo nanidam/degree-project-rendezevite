ce1a7bfc3f887ef3487a726eb15366253b8df775
Nani Dam <nani7dam@gmail.com>
2024-02-05 10:56:21 +0100

    :lock: redirect to not found

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: '/not-found',
+      },
+    ];
+}
   };
 
   module.exports = nextConfig;
