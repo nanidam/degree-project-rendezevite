70ed7bc5b0f19479eddee3ce10b6eba9b5ffd210
Nani Dam <nani7dam@gmail.com>
2024-02-05 15:22:14 +0100

    :sparkles: added guest RSVP

--- app/invitation/[inviteCode]/rsvp/page.tsx
+++ app/invitation/[inviteCode]/rsvp/page.tsx
@@ -1,13 +1,12 @@
 import { checkAccess } from "@/app/utils/checkAccess";
 import Rsvp from "@/app/utils/components/rsvp";
-import { redirect } from "next/navigation";
 
 const RsvpPage = async ({ params: { inviteCode } }: { params: { inviteCode: string } }) => {
   const info = await checkAccess(inviteCode);
 
   return (
     <>
-      <Rsvp guest={info.guest} />
+      <Rsvp guest={info.guest} eventId={info.event.id} eventName={info.event.eventName} />
     </>
   );
 };
