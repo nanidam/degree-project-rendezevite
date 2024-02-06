6fa8e681c85ae861ae247d76b337869dbf75981f
nanidam <nani.dam@medieinstitutet.se>
2024-02-06 22:29:39 +0100

    :lipstick: added envelope background to desktop view

--- app/utils/components/guestWelcome.tsx
+++ app/utils/components/guestWelcome.tsx
@@ -5,6 +5,7 @@ import { IEvent } from "../models/IEvent";
 import { ISession } from "../models/ISession";
 import { useRouter } from "next/navigation";
 import { ReactSVG } from "react-svg";
+import Image from "next/image";
 
 interface IGuestWelcomeProps {
   event: IEvent;
@@ -18,40 +19,52 @@ const GuestWelcome = ({ event, session }: IGuestWelcomeProps) => {
     <>
       <section className="welcome-bg">
         <article className="welcome-container">
-          <h1 className="welcome-header">
-            Welcome to <br />
-            {event.eventName}
-          </h1>
-          <p className="sub-header">{event.eventDate}</p>
-          <p className="invitation-welcome-text">A special invitation to:</p>
-          <article className="invitation-guests">
-            <span className="invitation-span">
-              <p className="invitation-guestname"> {event.guestList[0].name}</p>
-              {event.guestList[0].additionalGuest.name.length > 0 && (
-                <>
-                  <p className="additional-guest-prefix"> &</p>
-                  <p className="additional-guestname">
-                    {event.guestList[0].additionalGuest.name}
-                  </p>
-                </>
-              )}
-            </span>
-          </article>
+          <Image
+            className="gemetric-desktop"
+            src="/geometricDesignDesktop.png"
+            width={650}
+            height={500}
+            alt="Geometric background Design"
+            priority={true}
+            placeholder="empty"
+          />
+          <ReactSVG className="invitation-envelope" src="/svgs/invitation-envelope.svg" />
+          <div className="welcome-wrapper">
+            <h1 className="welcome-header">
+              Welcome to <br className="welcome-br" />
+              {event.eventName}
+            </h1>
+            <p className="sub-header">{event.eventDate}</p>
+            <p className="invitation-welcome-text">A special invitation to:</p>
+            <article className="invitation-guests">
+              <span className="invitation-span">
+                <p className="invitation-guestname"> {event.guestList[0].name}</p>
+                {event.guestList[0].additionalGuest.name.length > 0 && (
+                  <>
+                    <p className="additional-guest-prefix"> &</p>
+                    <p className="additional-guestname">
+                      {event.guestList[0].additionalGuest.name}
+                    </p>
+                  </>
+                )}
+              </span>
+            </article>
 
-          <div className="next-return-wrapper">
-            <button
-              className="invitation-next-btn"
-              type="button"
-              onClick={() => {
-                router.push(`/invitation/${event.id}/${event.eventName}`);
-              }}
-            >
-              <ReactSVG
-                className="invitation-navigation-arrow"
-                src="/svgs/arrow.svg"
-                aria-label="Next page of invitation"
-              ></ReactSVG>
-            </button>
+            <div className="next-return-wrapper">
+              <button
+                className="invitation-next-btn"
+                type="button"
+                onClick={() => {
+                  router.push(`/invitation/${event.id}/${event.eventName}`);
+                }}
+              >
+                <ReactSVG
+                  className="invitation-navigation-arrow"
+                  src="/svgs/arrow.svg"
+                  aria-label="Next page of invitation"
+                ></ReactSVG>
+              </button>
+            </div>
           </div>
         </article>
       </section>
--- app/utils/components/rsvp.tsx
+++ app/utils/components/rsvp.tsx
@@ -6,6 +6,7 @@ import { IGuest } from "../models/IGuest";
 import { rsvp } from "../rsvp";
 import { useRouter } from "next/navigation";
 import { ReactSVG } from "react-svg";
+import Image from "next/image";
 
 interface RsvpProps {
   guest: IGuest;
@@ -51,227 +52,240 @@ const Rsvp = ({ guest, eventId, eventName }: RsvpProps) => {
   return (
     <>
       <section className="template-bg">
-        <article className="template-container ">
-          {guest.hasResponded ? (
-            <h1 className="template-header">Response Received</h1>
-          ) : (
-            <>
-              <h1 className="template-header">Attending?</h1>
-            </>
-          )}
-          <form className="rsvp-form" onSubmit={handleRsvp}>
-            <div className="form-container">
-              <p className="guest-name">Guest: {guest.name}</p>
+        <article className="template-container">
+          <Image
+            className="gemetric-desktop"
+            src="/geometricDesignDesktop.png"
+            width={650}
+            height={500}
+            alt="Geometric background Design"
+            priority={true}
+            placeholder="empty"
+          />
+          <ReactSVG className="invitation-envelope" src="/svgs/invitation-envelope.svg" />
 
-              <div className="rsvp-attending">
-                <label htmlFor="attending" className="guest-label">
-                  Attending:
+          <div className="template-wrapper">
+            {guest.hasResponded ? (
+              <h1 className="template-header">Responded</h1>
+            ) : (
+              <>
+                <h1 className="template-header">Attending?</h1>
+              </>
+            )}
+
+            <form className="rsvp-form" onSubmit={handleRsvp}>
+              <div className="form-container">
+                <p className="guest-name">Guest: {guest.name}</p>
+
+                <div className="rsvp-attending">
+                  <label htmlFor="attending" className="guest-label">
+                    Attending:
+                  </label>
+                  <input
+                    className="rsvp-radio-input"
+                    type="radio"
+                    name="attending"
+                    value="true"
+                    disabled={guestState.hasResponded}
+                    defaultChecked={guestState.attending === guestState.hasResponded}
+                    required
+                  />
+                  <span className="checkbox-option">Yes</span>
+                  <input
+                    className="rsvp-radio-input"
+                    type="radio"
+                    name="attending"
+                    value="false"
+                    disabled={guestState.hasResponded}
+                    defaultChecked={!guestState.attending === guestState.hasResponded}
+                    required
+                  />
+                  <span className="checkbox-option">No</span>
+                </div>
+
+                <label className="rsvp-label" htmlFor="phoneNumber">
+                  Phone:
                 </label>
                 <input
-                  className="rsvp-radio-input"
-                  type="radio"
-                  name="attending"
-                  value="true"
+                  className="rsvp-input"
+                  type="text"
+                  name="phoneNumber"
+                  placeholder="07XX XXX XXX"
                   disabled={guestState.hasResponded}
-                  defaultChecked={guestState.attending === guestState.hasResponded}
+                  defaultValue={guestState.phoneNumber ?? ""}
                   required
                 />
-                <span className="checkbox-option">Yes</span>
-                <input
-                  className="rsvp-radio-input"
-                  type="radio"
-                  name="attending"
-                  value="false"
-                  disabled={guestState.hasResponded}
-                  defaultChecked={!guestState.attending === guestState.hasResponded}
-                  required
-                />
-                <span className="checkbox-option">No</span>
-              </div>
 
-              <label className="rsvp-label" htmlFor="phoneNumber">
-                Phone:
-              </label>
-              <input
-                className="rsvp-input"
-                type="text"
-                name="phoneNumber"
-                placeholder="07XX XXX XXX"
-                disabled={guestState.hasResponded}
-                defaultValue={guestState.phoneNumber ?? ""}
-                required
-              />
+                <div className="rsvp-diet">
+                  <label htmlFor="diet" className="guest-label">
+                    Diet:
+                  </label>
+                  <br />
+                  <input
+                    className="rsvp-radio-input"
+                    type="radio"
+                    name="diet"
+                    value="meat"
+                    defaultChecked={guestState.diet === "meat"}
+                    disabled={guestState.hasResponded}
+                    required
+                  />
+                  <span className="checkbox-option">Meat</span>
+                  <br />
+                  <input
+                    className="rsvp-radio-input"
+                    type="radio"
+                    name="diet"
+                    value="vegetarian"
+                    defaultChecked={guestState.diet === "vegetarian"}
+                    disabled={guestState.hasResponded}
+                    required
+                  />
+                  <span className="checkbox-option">Vegetarian</span>
+                  <br />
+                  <input
+                    className="rsvp-radio-input"
+                    type="radio"
+                    name="diet"
+                    value="vegan"
+                    defaultChecked={guestState.diet === "vegan"}
+                    disabled={guestState.hasResponded}
+                    required
+                  />
+                  <span className="checkbox-option">Vegan</span>
+                </div>
 
-              <div className="rsvp-diet">
-                <label htmlFor="diet" className="guest-label">
-                  Diet:
+                <label className="rsvp-label" htmlFor="allergies">
+                  Allergies:
                 </label>
-                <br />
                 <input
-                  className="rsvp-radio-input"
-                  type="radio"
-                  name="diet"
-                  value="meat"
-                  defaultChecked={guestState.diet === "meat"}
+                  className="rsvp-input"
+                  type="text"
+                  name="allergies"
+                  placeholder="Allergies"
+                  defaultValue={guestState.allergies ?? ""}
                   disabled={guestState.hasResponded}
-                  required
+                  maxLength={24} // TODO: adapt to this template DISPLAY max char available
                 />
-                <span className="checkbox-option">Meat</span>
-                <br />
+
+                <label className="rsvp-label" htmlFor="comments">
+                  Comments:
+                </label>
                 <input
-                  className="rsvp-radio-input"
-                  type="radio"
-                  name="diet"
-                  value="vegetarian"
-                  defaultChecked={guestState.diet === "vegetarian"}
+                  className="rsvp-input"
+                  type="text"
+                  name="comments"
+                  placeholder="Additional info"
+                  defaultValue={guestState.comments ?? ""}
                   disabled={guestState.hasResponded}
-                  required
+                  maxLength={24}
                 />
-                <span className="checkbox-option">Vegetarian</span>
-                <br />
-                <input
-                  className="rsvp-radio-input"
-                  type="radio"
-                  name="diet"
-                  value="vegan"
-                  defaultChecked={guestState.diet === "vegan"}
-                  disabled={guestState.hasResponded}
-                  required
-                />
-                <span className="checkbox-option">Vegan</span>
+
+                {guest.additionalGuest.name.length > 0 && (
+                  <>
+                    <div className="additional-guest">
+                      <p className="guest-name">Guest 1+: {guest.additionalGuest.name}</p>
+
+                      <div className="rsvp-attending">
+                        <label className="rsvp-label" htmlFor="additional-guest-attending">
+                          Attending:
+                        </label>
+                        <input
+                          className="rsvp-radio-input"
+                          type="radio"
+                          id="additional-guest-attending"
+                          name="additional-guest-attending"
+                          value="true"
+                          disabled={guestState.hasResponded}
+                          defaultChecked={guestState.additionalGuest.attending}
+                        />
+                        <span className="checkbox-option">Yes</span>
+                        <input
+                          className="rsvp-radio-input"
+                          type="radio"
+                          id="additional-guest-not-attending"
+                          name="additional-guest-not-attending"
+                          value="false"
+                          disabled={guestState.hasResponded}
+                          defaultChecked={!guestState.additionalGuest.attending}
+                        />
+                        <span className="checkbox-option">No</span>
+                      </div>
+
+                      <div className="rsvp-diet">
+                        <label className="guest-label" htmlFor="additional-guest-diet">
+                          Diet:
+                        </label>
+                        <br />
+                        <input
+                          className="rsvp-radio-input"
+                          type="radio"
+                          name="additional-guest-diet"
+                          value="meat"
+                          defaultChecked={guestState.additionalGuest.diet === "meat"}
+                          disabled={guestState.hasResponded}
+                        />
+                        <span className="checkbox-option">Meat</span>
+                        <br />
+                        <input
+                          className="rsvp-radio-input"
+                          type="radio"
+                          name="additional-guest-diet"
+                          value="vegetarian"
+                          disabled={guestState.hasResponded}
+                          defaultChecked={guestState.additionalGuest.diet === "vegetarian"}
+                        />
+                        <span className="checkbox-option">Vegetarian</span>
+                        <br />
+                        <input
+                          className="rsvp-radio-input"
+                          type="radio"
+                          name="additional-guest-diet"
+                          value="vegan"
+                          disabled={guestState.hasResponded}
+                          defaultChecked={guestState.additionalGuest.diet === "vegan"}
+                        />
+                        <span className="checkbox-option">Vegan</span>
+                      </div>
+
+                      <label className="rsvp-label" htmlFor="additional-guest-allergies">
+                        Allergies:
+                      </label>
+                      <input
+                        className="rsvp-input"
+                        type="text"
+                        name="additional-guest-allergies"
+                        placeholder="Allergies"
+                        disabled={guestState.hasResponded}
+                        defaultValue={guestState.additionalGuest.allergies ?? ""}
+                        maxLength={24}
+                      />
+                    </div>
+                  </>
+                )}
               </div>
+            </form>
 
-              <label className="rsvp-label" htmlFor="allergies">
-                Allergies:
-              </label>
-              <input
-                className="rsvp-input"
-                type="text"
-                name="allergies"
-                placeholder="Allergies"
-                defaultValue={guestState.allergies ?? ""}
-                disabled={guestState.hasResponded}
-                maxLength={24} // TODO: adapt to this template DISPLAY max char available
-              />
-
-              <label className="rsvp-label" htmlFor="comments">
-                Comments:
-              </label>
-              <input
-                className="rsvp-input"
-                type="text"
-                name="comments"
-                placeholder="Additional info"
-                defaultValue={guestState.comments ?? ""}
-                disabled={guestState.hasResponded}
-                maxLength={24}
-              />
-
-              {guest.additionalGuest.name.length > 0 && (
-                <>
-                  <div className="additional-guest">
-                    <p className="guest-name">Guest 1+: {guest.additionalGuest.name}</p>
-
-                    <div className="rsvp-attending">
-                      <label className="rsvp-label" htmlFor="additional-guest-attending">
-                        Attending:
-                      </label>
-                      <input
-                        className="rsvp-radio-input"
-                        type="radio"
-                        id="additional-guest-attending"
-                        name="additional-guest-attending"
-                        value="true"
-                        disabled={guestState.hasResponded}
-                        defaultChecked={guestState.additionalGuest.attending}
-                      />
-                      <span className="checkbox-option">Yes</span>
-                      <input
-                        className="rsvp-radio-input"
-                        type="radio"
-                        id="additional-guest-not-attending"
-                        name="additional-guest-not-attending"
-                        value="false"
-                        disabled={guestState.hasResponded}
-                        defaultChecked={!guestState.additionalGuest.attending}
-                      />
-                      <span className="checkbox-option">No</span>
-                    </div>
-
-                    <div className="rsvp-diet">
-                      <label className="guest-label" htmlFor="additional-guest-diet">
-                        Diet:
-                      </label>
-                      <br />
-                      <input
-                        className="rsvp-radio-input"
-                        type="radio"
-                        name="additional-guest-diet"
-                        value="meat"
-                        defaultChecked={guestState.additionalGuest.diet === "meat"}
-                        disabled={guestState.hasResponded}
-                      />
-                      <span className="checkbox-option">Meat</span>
-                      <br />
-                      <input
-                        className="rsvp-radio-input"
-                        type="radio"
-                        name="additional-guest-diet"
-                        value="vegetarian"
-                        disabled={guestState.hasResponded}
-                        defaultChecked={guestState.additionalGuest.diet === "vegetarian"}
-                      />
-                      <span className="checkbox-option">Vegetarian</span>
-                      <br />
-                      <input
-                        className="rsvp-radio-input"
-                        type="radio"
-                        name="additional-guest-diet"
-                        value="vegan"
-                        disabled={guestState.hasResponded}
-                        defaultChecked={guestState.additionalGuest.diet === "vegan"}
-                      />
-                      <span className="checkbox-option">Vegan</span>
-                    </div>
-
-                    <label className="rsvp-label" htmlFor="additional-guest-allergies">
-                      Allergies:
-                    </label>
-                    <input
-                      className="rsvp-input"
-                      type="text"
-                      name="additional-guest-allergies"
-                      placeholder="Allergies"
-                      disabled={guestState.hasResponded}
-                      defaultValue={guestState.additionalGuest.allergies ?? ""}
-                      maxLength={24}
-                    />
-                  </div>
-                </>
+            <div className="send-return-wrapper">
+              <button
+                className="invitation-btn"
+                type="button"
+                onClick={() => {
+                  router.push(`/invitation/${eventId}/${eventName}`);
+                }}
+              >
+                <ReactSVG
+                  className="invitation-navigation-arrow"
+                  src="/svgs/arrow.svg"
+                  aria-label="Return"
+                ></ReactSVG>
+              </button>
+              {!guest.hasResponded && (
+                <button className="rsvp-submit-btn" type="submit">
+                  Send
+                </button>
               )}
             </div>
-          </form>
-          {!guest.hasResponded && (
-            <div className="rsvp-btn-container">
-              <button className="rsvp-submit-btn" type="submit">
-                Send
-              </button>
-            </div>
-          )}
-          <div className="next-return-wrapper">
-            <button
-              className="invitation-btn"
-              type="button"
-              onClick={() => {
-                router.push(`/invitation/${eventId}/${eventName}`);
-              }}
-            >
-              <ReactSVG
-                className="invitation-navigation-arrow"
-                src="/svgs/arrow.svg"
-                aria-label="Return"
-              ></ReactSVG>
-            </button>
           </div>
         </article>
       </section>
