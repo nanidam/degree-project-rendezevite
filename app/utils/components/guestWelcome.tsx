0b8e79ae67c55f18029366024cad1044e976140e
nanidam <nani.dam@medieinstitutet.se>
2024-02-06 15:00:03 +0100

    added guest invitation welcome page mobile

--- app/utils/components/guestWelcome.tsx
+++ app/utils/components/guestWelcome.tsx
@@ -1,8 +1,10 @@
 "use client";
+
+import "./style/guestWelcome.scss";
 import { IEvent } from "../models/IEvent";
 import { ISession } from "../models/ISession";
 import { useRouter } from "next/navigation";
-import "./style/guestWelcome.scss";
+import { ReactSVG } from "react-svg";
 
 interface IGuestWelcomeProps {
   event: IEvent;
@@ -14,32 +16,44 @@ const GuestWelcome = ({ event, session }: IGuestWelcomeProps) => {
   const router = useRouter();
   return (
     <>
-      <section className="wrapper-invitation">
-        <h1 className="welcome-title">Welcome to {event.eventName}</h1>
-        <h4 className="sub-title">{event.eventDate}</h4>
-        <p className="welcome-text">A special invitation to:</p>
-        <span className="welcome-span">
-          <p className="guest-name"> {event.guestList[0].name}</p>
-          {event.guestList[0].additionalGuest.name.length > 0 && (
-            <>
-              <p className="additional-guest-prefix"> &</p>
-              <p className="additional-guest-name">
-                {event.guestList[0].additionalGuest.name}
-              </p>
-            </>
-          )}
-        </span>
-        <div className="next-return-wrapper">
-          <button
-            className="submit-event-btn"
-            type="button"
-            onClick={() => {
-              router.push(`/invitation/${event.id}/${event.eventName}`);
-            }}
-          >
-            Next
-          </button>
-        </div>
+      <section className="template-bg ">
+        <article className="template-container">
+          <h1 className="template-header">
+            Welcome to <br />
+            {event.eventName}
+          </h1>
+          <p className="sub-header">{event.eventDate}</p>
+          <p className="invitation-welcome-text">A special invitation to:</p>
+          <article className="invitation-guests">
+            <span className="invitation-span">
+              <p className="invitation-guestname"> {event.guestList[0].name}</p>
+              {event.guestList[0].additionalGuest.name.length > 0 && (
+                <>
+                  <p className="additional-guest-prefix"> &</p>
+                  <p className="additional-guestname">
+                    {event.guestList[0].additionalGuest.name}
+                  </p>
+                </>
+              )}
+            </span>
+          </article>
+
+          <div className="next-return-wrapper">
+            <button
+              className="invitation-next-btn"
+              type="button"
+              onClick={() => {
+                router.push(`/invitation/${event.id}/${event.eventName}`);
+              }}
+            >
+              <ReactSVG
+                className="invitation-navigation-arrow"
+                src="/svgs/arrow.svg"
+                aria-label="Next page of invitation"
+              ></ReactSVG>
+            </button>
+          </div>
+        </article>
       </section>
     </>
   );
