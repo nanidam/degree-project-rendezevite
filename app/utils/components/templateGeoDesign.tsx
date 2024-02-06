6755577ba9d8f9b49b3a2f59f67bbf2ede32356e
Nani Dam <nani7dam@gmail.com>
2024-02-06 16:35:09 +0100

    :fire: removed comments

--- app/utils/components/templateGeoDesign.tsx
+++ app/utils/components/templateGeoDesign.tsx
@@ -9,74 +9,8 @@ const TemplateGeometricDesign: React.FC<ITemplateGeometricDesignProps> = ({
   header,
   text,
   eventId,
-  eventName,
 }) => {
   const router = useRouter();
-  // TODO: Fix guest name
-  // const guestName = "Test Person 1";
-
-  // const handleRsvp = (event: React.FormEvent<HTMLFormElement>) => {
-  //   event.preventDefault();
-  //   console.log("RSVP CLICK!!! ");
-  // };
-  // const renderRSVPContent = () => (
-  //   <>
-  //     <form className="rsvp-form" onSubmit={handleRsvp}>
-  //       <label htmlFor="guest">
-  //         Guest:
-  //         <input
-  //           className="rsvp-input"
-  //           type="text"
-  //           id="guest"
-  //           name="guest"
-  //           value={guestName}
-  //           readOnly
-  //         />
-  //       </label>
-
-  //       <div className="rsvp-wrapper">
-  //         <label htmlFor="attending">
-  //           {" "}
-  //           Attending: <br />
-  //           <input type="radio" name="attending" value="yes" /> Yes
-  //           <input type="radio" name="attending" value="no" /> No
-  //         </label>
-  //       </div>
-
-  //       <div className="rsvp-wrapper">
-  //         <label className="diet-label" htmlFor="diet">
-  //           Diet: <br />
-  //           <input className="rsvp-input" type="radio" name="diet" value="meat" /> Meat
-  //           <input
-  //             className="rsvp-input"
-  //             type="radio"
-  //             name="diet"
-  //             value="vegan"
-  //           /> Vegan <br />
-  //           <input
-  //             className="rsvp-input"
-  //             type="radio"
-  //             name="diet"
-  //             value="vegetarian"
-  //           />{" "}
-  //           Vegetarian
-  //         </label>
-  //       </div>
-
-  //       <label htmlFor="allergies">
-  //         Allergies:
-  //         <input className="rsvp-input" type="text" id="allergies" name="allergies" />
-  //       </label>
-
-  //       <label htmlFor="comments">
-  //         Comments:
-  //         <input className="rsvp-input" type="text" id="comments" name="comments" />
-  //       </label>
-
-  //       <button className="rsvp-btn">Send</button>
-  //     </form>
-  //   </>
-  // );
 
   return (
     <section className="template-bg">
