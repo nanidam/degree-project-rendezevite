d0f692137f5f32655e778e41159b35814ac61825
nanidam <nani.dam@medieinstitutet.se>
2024-02-06 19:07:19 +0100

    :lipstick: styled not found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,6 +1,21 @@
+"use client";
+
+import { ReactSVG } from "react-svg";
+import "./style.scss";
+
 const NotFound = () => {
-  return <h1>Page not found</h1>;
+  return (
+    <section className="not-found">
+      <h1 className="not-found-title">Page not found</h1>
+      <article className="not-found-wrapper">
+        <ReactSVG src="/svgs/page-not-found.svg" />
+        <p className="not-found-text">We could not find the page you were looking for.</p>
+        <p className="not-found-text">Please check the URL and try again.</p>
+        <a className="return-to-main-btn" href="/">
+          To main page
+        </a>
+      </article>
+    </section>
+  );
 };
 export default NotFound;
-
-// TODO: style this
