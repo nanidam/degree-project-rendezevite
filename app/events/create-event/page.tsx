23631e9ca628b64c08e79d4e79eb116e32cb1419
Nani Dam <nani7dam@gmail.com>
2024-02-07 04:01:41 +0100

    :bug: fix redirect to unauthorized if not admin login

--- app/events/create-event/page.tsx
+++ app/events/create-event/page.tsx
@@ -1,10 +1,11 @@
-import getUserId from "@/app/services/getUserId";
 import "./style.scss";
+import getUserId from "@/app/services/getUserId";
 import CreateEditEvent from "@/app/utils/components/createEditEvent";
+import { redirect } from "next/navigation";
 
 const CreateEvent = async () => {
   const userId = await getUserId();
-  if (!userId) return;
+  if (!userId) redirect("/unauthorized");
   return <CreateEditEvent userId={userId} />;
 };
 
