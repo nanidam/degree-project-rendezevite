0e073b9d6503173f38b08898bf20af1af077bee1
nanidam <nani.dam@medieinstitutet.se>
2024-02-09 00:24:11 +0100

    :bug: fixed login error msg

--- app/utils/login.ts
+++ app/utils/login.ts
@@ -2,30 +2,34 @@ import { signIn } from "next-auth/react";
 import { useRouter } from "next/navigation";
 
 interface ILogin {
-    e: React.FormEvent<HTMLFormElement>;
-    loginType: string;
-    eventId?: string;
-    router: ReturnType<typeof useRouter>;
-    setErrorMsg: React.Dispatch<React.SetStateAction<string | undefined>>;
+  e: React.FormEvent<HTMLFormElement>;
+  loginType: string;
+  eventId?: string;
+  router: ReturnType<typeof useRouter>;
+  setErrorMsg: React.Dispatch<React.SetStateAction<string | undefined>>;
 }
 const login = async ({ e, loginType, eventId, router, setErrorMsg }: ILogin) => {
-    e.preventDefault();
-    const email = e.currentTarget.email.value as string;
-    const password = e.currentTarget.password.value as string;
+  e.preventDefault();
+  const email = e.currentTarget.email.value as string;
+  const password = e.currentTarget.password.value as string;
 
-    const result = await signIn("credentials", {
-        email,
-        password,
-        loginType,
-        redirect: false,
-        eventId
-    });
+  const result = await signIn("credentials", {
+    email,
+    password,
+    loginType,
+    redirect: false,
+    eventId,
+  });
 
-    if (result?.status === 200 && loginType === "admin") router.push("/events/overview");
-    if (result?.status === 200 && loginType === "guest") router.push(`/invitation/${eventId}/welcome`);
-    if (result?.status === 401) setErrorMsg("Either you are not invited to this event or your email or password is incorrect")
-
-
+  if (result?.status === 200 && loginType === "admin") router.push("/events/overview");
+  if (result?.status === 200 && loginType === "guest")
+    router.push(`/invitation/${eventId}/welcome`);
+  if (result?.status === 401 && loginType === "guest")
+    setErrorMsg(
+      "Either you are not invited to this event or your email or password is incorrect"
+    );
+  if (result?.status === 401 && loginType === "admin")
+    setErrorMsg("Wrong email or password.");
 };
 
-export default login
+export default login;
